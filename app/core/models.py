c89d26ba15350450ba0ac748bcc0acdec8784265
bradley maxted <bax.maxted@gmail.com>
2024-01-20 00:30:53 -0700

    started to add user models

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,23 @@
-from django.db import models # noqa
+"""
+Datanase models
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    User in the system
+    Note default django user implementation uses a username
+    and password. The code implementation here replaces this
+    with an email and password requirement
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
